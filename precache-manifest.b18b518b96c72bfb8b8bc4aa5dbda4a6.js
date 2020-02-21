78485bd26c9419b45f6c596f3396d4bbfeaa681a
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-02-21 14:40:12 +0800

    Updates

---
+++ precache-manifest.b18b518b96c72bfb8b8bc4aa5dbda4a6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaca1f3237452f940ef192c6ae6352f2",
+    "url": "/chemakers-ans/index.html"
+  },
+  {
+    "revision": "723ffa233d6a36c0187a",
+    "url": "/chemakers-ans/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "33404b12951a5e1e8774",
+    "url": "/chemakers-ans/static/css/main.244db809.chunk.css"
+  },
+  {
+    "revision": "723ffa233d6a36c0187a",
+    "url": "/chemakers-ans/static/js/2.6244e594.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/chemakers-ans/static/js/2.6244e594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33404b12951a5e1e8774",
+    "url": "/chemakers-ans/static/js/main.de5d0f98.chunk.js"
+  },
+  {
+    "revision": "df375556d662eaccc591",
+    "url": "/chemakers-ans/static/js/runtime-main.2681ff82.js"
+  },
+  {
+    "revision": "59d983750cc34861abfb0f8645183b55",
+    "url": "/chemakers-ans/static/media/Graphik-Bold.59d98375.ttf"
+  },
+  {
+    "revision": "741c6248309abbad09752fa8df383cc7",
+    "url": "/chemakers-ans/static/media/Graphik-BoldItalic.741c6248.ttf"
+  },
+  {
+    "revision": "9d2f5686baa105598155b8737797be1d",
+    "url": "/chemakers-ans/static/media/Graphik-Medium.9d2f5686.ttf"
+  },
+  {
+    "revision": "1a199a3bc0bf598cf9b4b8908e45fc26",
+    "url": "/chemakers-ans/static/media/Graphik-MediumItalic.1a199a3b.ttf"
+  },
+  {
+    "revision": "db39fbca11f5102d09a6299df182fdf1",
+    "url": "/chemakers-ans/static/media/Graphik-Regular.db39fbca.ttf"
+  },
+  {
+    "revision": "231fc40a9b30a21ef3596a626a55546c",
+    "url": "/chemakers-ans/static/media/Graphik-RegularItalic.231fc40a.ttf"
+  },
+  {
+    "revision": "61d032e8148c2e9c521800f3294e4825",
+    "url": "/chemakers-ans/static/media/Shapiro 95 Super Wide.61d032e8.ttf"
+  },
+  {
+    "revision": "e81426ad5a4641fe6ef428ad1ee791be",
+    "url": "/chemakers-ans/static/media/sample.e81426ad.jpg"
+  }
+]);
