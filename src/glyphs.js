0440e24811138ad828fb8c78e805cb07733dfb40
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-02-22 14:56:20 +0800

    Added glyphs

---
+++ src/glyphs.js
@@ -0,0 +1,13 @@
+import arrowForward from "./assets/glyphs/arrow_forward-24px.svg";
+import backspace from "./assets/glyphs/backspace-24px.svg";
+import scan from "./assets/glyphs/scan.svg";
+import stop from "./assets/glyphs/stop-24px.svg";
+import close from "./assets/glyphs/close-24px.svg";
+
+export const glyphs = {
+    arrowForward: arrowForward,
+    backspace: backspace,
+    scan: scan,
+    stop: stop,
+    close: close
+}
