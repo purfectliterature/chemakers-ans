d83d5cd7ed503f0d5d02f4032f3f3d59206d2094
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-02-22 17:46:44 +0800

    Added answers

---
+++ src/answers.js
@@ -0,0 +1,103 @@
+import a01 from "./assets/answers/0_C06_01X.jpg";
+import a02 from "./assets/answers/0_C06_02X.jpg";
+import a03 from "./assets/answers/0_C06_03X.jpg";
+import a04 from "./assets/answers/0_C06_04X.jpg";
+import a05 from "./assets/answers/0_C06_05X.jpg";
+import a06 from "./assets/answers/0_C06_06X.jpg";
+import a07 from "./assets/answers/0_C06_07X.jpg";
+import a08 from "./assets/answers/0_C06_08X.jpg";
+import a11 from "./assets/answers/1_C09_01X.jpg";
+import a12 from "./assets/answers/1_C09_02X.jpg";
+import a13 from "./assets/answers/1_C09_03X.jpg";
+import a21 from "./assets/answers/2_C10_01X.jpg";
+import a22 from "./assets/answers/2_C10_02X.jpg";
+import a23 from "./assets/answers/2_C10_03X.jpg";
+import a24 from "./assets/answers/2_C10_04X.jpg";
+import a25 from "./assets/answers/2_C10_05X.jpg";
+import a26 from "./assets/answers/2_C10_06X.jpg";
+import a27 from "./assets/answers/2_C10_07X.jpg";
+import a28 from "./assets/answers/2_C10_08X.jpg";
+import a31 from "./assets/answers/3_C11_01X.jpg";
+import a32 from "./assets/answers/3_C11_02X.jpg";
+import a33 from "./assets/answers/3_C11_03X.jpg";
+import a34 from "./assets/answers/3_C11_04X.jpg";
+import a35 from "./assets/answers/3_C11_05X.jpg";
+import a36 from "./assets/answers/3_C11_06X.jpg";
+import a37 from "./assets/answers/3_C11_07X.jpg";
+import a38 from "./assets/answers/3_C11_08X.jpg";
+import a39 from "./assets/answers/3_C11_09X.jpg";
+import a310 from "./assets/answers/3_C11_10X.jpg";
+import a311 from "./assets/answers/3_C11_11X.jpg";
+import a312 from "./assets/answers/3_C11_12X.jpg";
+import a41 from "./assets/answers/4_C15_01X.jpg";
+import a42 from "./assets/answers/4_C15_02X.jpg";
+import a43 from "./assets/answers/4_C15_03X.jpg";
+import a44 from "./assets/answers/4_C15_04X.jpg";
+import a45 from "./assets/answers/4_C15_05X.jpg";
+import a46 from "./assets/answers/4_C15_06X.jpg";
+import a47 from "./assets/answers/4_C15_07X.jpg";
+import a48 from "./assets/answers/4_C15_08X.jpg";
+import a49 from "./assets/answers/4_C15_09X.jpg";
+import a410 from "./assets/answers/4_C15_10X.jpg";
+import a51 from "./assets/answers/5_CST_01X.jpg";
+import a52 from "./assets/answers/5_CST_02X.jpg";
+import a53 from "./assets/answers/5_CST_03X.jpg";
+import a54 from "./assets/answers/5_CST_04X.jpg";
+import a61 from "./assets/answers/6_CMI_01X.jpg";
+import a62 from "./assets/answers/6_CMI_02X.jpg";
+import a63 from "./assets/answers/6_CMI_03X.jpg";
+import a64 from "./assets/answers/6_CMI_04X.jpg";
+import a65 from "./assets/answers/6_CMI_05X.jpg";
+
+export const answers = {
+    '01': a01,
+    '02': a02,
+    '03': a03,
+    '04': a04,
+    '05': a05,
+    '06': a06,
+    '07': a07,
+    '08': a08,
+    '11': a11,
+    '12': a12,
+    '13': a13,
+    '21': a21,
+    '22': a22,
+    '23': a23,
+    '24': a24,
+    '25': a25,
+    '26': a26,
+    '27': a27,
+    '28': a28,
+    '31': a31,
+    '32': a32,
+    '33': a33,
+    '34': a34,
+    '35': a35,
+    '36': a36,
+    '37': a37,
+    '38': a38,
+    '39': a39,
+    '310': a310,
+    '311': a311,
+    '312': a312,
+    '41': a41,
+    '42': a42,
+    '43': a43,
+    '44': a44,
+    '45': a45,
+    '46': a46,
+    '47': a47,
+    '48': a48,
+    '49': a49,
+    '410': a410,
+    '51': a51,
+    '52': a52,
+    '53': a53,
+    '54': a54,
+    '61': a61,
+    '62': a62,
+    '63': a63,
+    '64': a64,
+    '65': a65
+};
